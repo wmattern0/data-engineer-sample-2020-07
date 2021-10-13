e2be85b0933d5711b7a22745b419ac9d5d9e31ed
William Mattern <wm.m.mattern@gmail.com>
2021-10-12 22:38:44 -0400

    writing output.csv, need to put header in order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import pathlib
 import src.some_storage_library
+import pprint
 
 """
 This is the entrypoint to the program. 'python main.py' will be executed and the 
@@ -9,19 +10,32 @@ expected csv file should exist in ../data/destination/ after the execution is co
 
 class DataEngineerSample:
 
+    def __init__(self):
+        self.data_eng_sample_data=''
+        self.data_eng_sample_header=''
+
     def process_header(self):
         header_file = open(pathlib.Path('data/source/SOURCECOLUMNS.txt'))
         header_reader = csv.reader(header_file, delimiter='|')
-        data_eng_sample_headers = list(header_reader)
-        print(data_eng_sample_headers)
+        # put header in order
+
+        self.data_eng_sample_header = dict(header_reader)
+        pprint.pprint(self.data_eng_sample_header)
 
     def process_data(self):
         data_file = open(pathlib.Path('data/source/SOURCEDATA.txt'))
         data_reader = csv.reader(data_file, delimiter='|')
-        data_eng_sample_data = list(data_reader)
-        print(data_eng_sample_data)
+        self.data_eng_sample_data = list(data_reader)
+        print(self.data_eng_sample_data)
 
-    def write_data_engineer_sample(self):
+    def write_data_engineer_sample_output(self):
+        writer = src.some_storage_library.SomeStorageLibrary()
+        outputFile = open('output.csv', 'w', newline='')
+        outputWriter = csv.writer(outputFile)
+        outputWriter.writerow(self.data_eng_sample_header)
+        for row in self.data_eng_sample_data:
+            outputWriter.writerow(row)
+        writer.load_csv('output.csv')
         pass
 
 
@@ -30,6 +44,6 @@ if __name__ == '__main__':
     print('Beginning the ETL process...')
     data_engineer_sample = DataEngineerSample()
     data_engineer_sample.process_header()
-    data_engineer_sample.process_data()
-    data_engineer_sample.write_data_engineer_sample()
+    #data_engineer_sample.process_data()
+    #data_engineer_sample.write_data_engineer_sample_output()
     print('Done the ETL process...')
