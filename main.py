89c45afd776109ed7db272e44f5b57e07f3d69c9
William Mattern <wm.m.mattern@gmail.com>
2021-10-12 21:46:04 -0400

    skeleton

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import csv
+import pathlib
+import src.some_storage_library
+
 """
 This is the entrypoint to the program. 'python main.py' will be executed and the 
 expected csv file should exist in ../data/destination/ after the execution is complete.
 """
 
+def process_header():
+    pass
 
+def process_data():
+    pass
+
+def write_data_engineer_sample():
+    pass
 
 if __name__ == '__main__':
     """Entrypoint"""
     print('Beginning the ETL process...')
+    process_header()
+    process_data()
+    write_data_engineer_sample()
+    print('Done the ETL process...')
